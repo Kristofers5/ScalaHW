31069b57dbec77941b0c0d7f112464262c959669
Kristofers5 <kristofers1104@inbox.lv>
2025-11-29 18:42:50 +0000

    29.11.2025

--- Main.scala
+++ Main.scala
@@ -1,23 +1,31 @@
 // 241RDB193 Kristofers Stūris
 
 
-def factor(n: Int): Unit = {
 
-    @annotation.tailrec
-    def helper(n: Int, factor: Int): Unit = {
-        if ( factor > n ){
-            return
-        }
 
-        if ( n % factor == 0) {
-            printf("%d ", factor)
-            helper(math.floor(n / factor).toInt, factor)
-        }else{
-            helper(n, factor + 1)
-        }
+case class Node(var left: Tree, var value: Int, var right: Tree)
+
+
+def pievienot(root: Node, value: Int): Node{
+    if (root == null){
+        Node(value)
+    }else if ( value < root.value){
+        root.left = pievienot(root.left, value)
+        return root
+    }else{
+        root.right = pievienot(root.right, value)
+        return root
     }
+}
 
-    helper(n, 2)
+
+//testing
+def inOrderTraversal(root: TreeNode): Unit = {
+  if (root != null) {
+    inOrderTraversal(root.left)
+    print(root.value + " ")
+    inOrderTraversal(root.right)
+  }
 }
 
 
@@ -30,13 +38,25 @@ object Main {
         printf("n = ")
         var n = scala.io.StdIn.readInt()
 
-        if (n < 2) {
-            printf("error")
-
-        }else{
-            factor(n)
-        }
-        println("")
+        
         
     }
+
+
+    //testing
+    var root: TreeNode = null
+
+    val values = List(50, 30, 70, 20, 40, 60, 80)
+
+    for (v <- values) {
+        root = insert(root, v)
+    }
+
+    println("In-order Traversal of BST:")
+    inOrderTraversal(root)
+
+
 }
+
+
+
