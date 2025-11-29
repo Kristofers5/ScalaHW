465fb9f1297e74ee884ac8e2ee86cf18137a5f3a
Kristofers5 <kristofers1104@inbox.lv>
2025-11-29 17:31:24 +0000

    md1

--- Main.scala
+++ Main.scala
@@ -1,6 +1,42 @@
-object Hello {
-    def main(args: Array[String]) = {
-        println("Hello, world")
-        println("SUCK IT YOU MOTHERFUCKING LANGUAGE ASSS WHY IS IT SO GODDAMN DIFFICULT TO JUST SIMPLY RUN THIS SHIT")
+// 241RDB193 Kristofers Stūris
+
+
+def factor(n: Int): Unit = {
+
+    @annotation.tailrec
+    def helper(n: Int, factor: Int): Unit = {
+        if ( factor > n ){
+            return
+        }
+
+        if ( n % factor == 0) {
+            printf("%d ", factor)
+            helper(math.floor(n / factor).toInt, factor)
+        }else{
+            helper(n, factor + 1)
+        }
+    }
+
+    helper(n, 2)
+}
+
+
+
+
+object Main {
+
+    def main(args: Array[String]): Unit = {
+
+        printf("n = ")
+        var n = scala.io.StdIn.readInt()
+
+        if (n < 2) {
+            printf("error")
+
+        }else{
+            factor(n)
+        }
+        println("")
+        
     }
 }
