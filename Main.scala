748a692a56c6ece049895d6092289e78360425b2
Kristofers5 <kristofers1104@inbox.lv>
2025-11-29 03:09:22 +0000

    AAAAAAAAAAA

---
+++ Main.scala
@@ -0,0 +1,6 @@
+object Hello {
+    def main(args: Array[String]) = {
+        println("Hello, world")
+        println("SUCK IT YOU MOTHERFUCKING LANGUAGE ASSS WHY IS IT SO GODDAMN DIFFICULT TO JUST SIMPLY RUN THIS SHIT")
+    }
+}
